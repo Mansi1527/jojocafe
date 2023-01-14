a52dfe0bc5e070c60f4e856c6a39b895ff5c0d65
Mansi1527 <122034236+Mansi1527@users.noreply.github.com>
2023-01-14 20:08:35 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,73 +1,17 @@
-!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JOJO Cafe</title>
+<?php    
+$to = "mansiy1527@icloud.com";
+$subject = "form";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$number = $_POST["number"];
+$mes = $_POST["message"];
+$message = "name :" .$name . "\n" . "email :" .$email . "\n" ."number :" .$number ."\n" ."message :" .$mes;
+ if(mail($to,$subject, $message)){
+    echo 'your msg sent';
+ }
+ else{
+    echo 'unable to send';
+ }
 
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
-    <!-- font awesome cdn link  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-    <!-- custom css file link  -->
-    <link rel="stylesheet" href="style.css">
-
-</head>
-<body>
-
-<?php
-//variable setting
-if(!empty($_POST["send"])){
-    $name=$_POST["name"];
-    $email=$_POST["email"];
-    $number=$_POST["number"];
-    $message=$_POST["message"];
-    $toemail="mansiy1527@gmail.com";
-
-    $mailheaders = "Name :" . $name .
-"\r\n Email :" . $email .
-"\r\n Number :" . $number .
-"\r\n message :" . $message . "\r\n";
-if(mail($toemail, $name,$mailheaders)){
-  $message ="your message is received";
-}
-
-
-
-}
 
 ?>
-<section class="book" id="book">
-
-<h1 class="heading"> booking <span>reserve a table</span> </h1>
-
-<form action="mansiy1527@icloud.com" method="POST">
-    <label for="name"></label>
-    <input type="text" placeholder="your name" class="box" name="name">
-    <label for="email"></label>
-    <input type="email" placeholder="your email" class="box" name="email">
-    <label for="number"></label>
-    <input type="number" placeholder="your number" class="box" name="number">
-    <label for="message"></label>
-    <textarea name="" placeholder="your message" class="box" id="" cols="30" rows="10" name="message"></textarea>
-
-    <input type="submit" value="Reserve" class="btn" name="send">
-    <?php 
-    if(!empty($message)){?>
-    <div class="success">
-<strong><?php echo &message; ?></strong>
-
-    </div>
-
-    <?php}
-    ?>
-
-</form>
-
-</section>
-
-
-
-</body>
